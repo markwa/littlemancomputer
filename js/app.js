845e23a0d4fcd1b60e2c2d4c4792de4f190ec994
Mark Waller <mark@qwiqr.co.uk>
2025-02-10 12:29:24 +0000

    first working version of the assembler

--- js/app.js
+++ js/app.js
@@ -1,65 +1,158 @@
 // App
-  const { createApp, ref } = Vue
+const {createApp, ref} = Vue
 
-  createApp({
-    setup() {
-    },
-    data : function () {
-      return {
-        ramarray: new Array(100).fill(0),
-        pc: 0,
-        mar: 0,
-        cir: 0,
-        mdr: 0,
-        acc: 0,
-        code: "      INP\n" +
-              "      STA 99\n" +
-              "      INP\n" +
-              "      ADD 99\n" +
-              "      OUT\n" +
-              "      HLT\n"+
-              " data DAT",
-        input: "13",
-        output: "15",
-        codeselect: "add",
+createApp({
+  setup() {
+  },
+  data: function () {
+    return {
+      ramarray: new Array(100).fill(0),
+      pc: 0,
+      mar: 0,
+      cir: 0,
+      mdr: 0,
+      acc: 0,
+      code: "      INP\n" +
+        "      STA 99\n" +
+        "      INP\n" +
+        "      ADD 99\n" +
+        "      OUT\n" +
+        "      HLT\n" +
+        " data DAT",
+      input: "13",
+      output: "15",
+      codeselect: "add",
+    }
+  },
+  methods: {
+    formatInt: function (num, places) {
+      if (num < 0) {
+        return '-' + String(Math.abs(num)).padStart(places, '0')
+      } else {
+        return String(num).padStart(places, '0')
       }
     },
-    methods: {
-      formatInt: function( num , places ){
-          if ( num < 0 ){
-            return '-'+String(Math.abs(num)).padStart(places, '0')
-          } else {
-            return String(num).padStart(places, '0')
-          }
-      },
 
-      assembleCodeToRam: function() {
-        // define the opcodes
-        const mnenomics = ["hlt","add","sub","sta","!!!","lda","bra","brz","brp","inp","out","otc","dat"];
+    assembleCodeToRam: function () {
+      // define the opcodes
+      const mnenomics = ["hlt", "add", "sub", "sta", "!!!", "lda", "bra", "brz", "brp", "inp", "out", "otc", "dat"];
 
-        let labels = {} ;
-        const lines = this.code.split('\n');
+      let labels = {};
+      const lines = this.code.split('\n');
 
-        // perform an initial pass looking for labels
-        for (var i = 0; i < lines.length; i++) {
-          var line = lines[i].trim();
-          const parts = line.split(' ');
-          var opcode = -1;
-          for (var j = 0; j < parts.length; j++) {
-            var part = parts[j].trim();
-            part = part.toLowerCase();
-            if( part !== '' && opcode < 0 ) {
-              let index = mnenomics.indexOf(part);
-              if (index >= 0) {
-                opcode = index;
-              } else {
-                labels[i]=part;
-              }
+      // first pass looking for labels
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        const parts = line.split(' ');
+        var opcode = -1;
+        for (var j = 0; j < parts.length; j++) {
+          var part = parts[j].trim();
+          part = part.toLowerCase();
+          if (part !== '' && opcode < 0) {
+            let index = mnenomics.indexOf(part);
+            if (index >= 0) {
+              opcode = index;
+            } else {
+              labels[part] = i;
             }
           }
         }
+      }
 
-        console.log( labels )
-      },
-    }
-  }).mount('#app')
+      // second pass assemble into memarray
+      var memarray = new Array();
+      var error = null;
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        const parts = line.split(' ');
+        var opcode = -1;
+        var operand = 0;
+        for (var j = 0; j < parts.length; j++) {
+          var part = parts[j].trim();
+          part = part.toLowerCase();
+
+          let index = mnenomics.indexOf(part);
+          if (index >= 0) {
+            opcode = index;
+
+            // is there data and is it a label
+            let data = null;
+            if (j < (parts.length - 1)) {
+              data = parts[j + 1].trim();
+              data = data.toLowerCase();
+            }
+
+            // what should the operand be
+            switch (opcode) {
+              case 0: // hlt - no operand
+              case 6: // bra - no operand
+                break;
+              case 4:
+                error = "Invalid mnemonic on line ${i}";
+                return error;
+              case 1: // add
+              case 2: // sub
+              case 3: // sta
+              case 5: // lda
+              case 7: // brz
+              case 8: // brp
+                // operand is label or direct address
+                if (data) {
+                  if (labels[data] !== undefined) operand = labels[data];
+                  else operand = parseInt(data);
+                  if (isNaN(operand)) {
+                    error = "Invalid address on line ${i}";
+                    return error;
+                  }
+                } else {
+                  error = "Missing address/label on line ${i}";
+                  return error;
+                }
+                break;
+              case 9: //inp
+                operand = 1;
+                break;
+              case 10: //out
+                opcode = 9;
+                operand = 2;
+                break;
+              case 11:
+                opcode = 9;
+                operand = 22;
+                break;
+              case 12: //dat
+                opcode = 0;
+                if (data) {
+                  operand = parseInt(data);
+                  if (isNaN(operand)) {
+                    error = "Invalid value on line ${i}";
+                    return error;
+                  }
+                }
+                break;
+              default:
+                error = "Invalid mnemonic on line ${i}";
+                return error;
+            }
+
+            // combine opcode and operand
+            let instruction = (opcode * 100) + operand;
+
+            // add instruction to the memarray
+            memarray.push(instruction);
+          }
+        }
+      }
+
+      // clear contains of ram
+      for (var i = 0; i < 100; i++) {
+        this.ramarray[i] = 0;
+      }
+
+      // copy the data to the ram
+      for (var i = 0; i < memarray.length && i < 100; i++) {
+        this.ramarray[i] = memarray[i];
+      }
+    },
+  }
+}).mount('#app')
